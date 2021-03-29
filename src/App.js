46c1f1b97e090fbf0903b30862c80ac420f15441
rheophile10 <rheophile10@gmail.com>
2021-03-28 18:51:16 -0600

    adding dos and don'ts

--- src/App.js
+++ src/App.js
@@ -1,27 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API } from 'aws-amplify';
+import { listTodos } from './graphql/queries';
+import { createTodo as createTodoMutation, updateTodo as updateTodoMutation } from './graphql/mutations';
+import { useEffect, useState } from 'react';
+
+const initialFormState = { action: '' }
 
 function App() {
+  const [Todos, setTodos] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  async function fetchTodos() {
+    const apiData = await API.graphql({ query: listTodos });
+    setTodos(apiData.data.listTodos.items);
+  }
+
+  async function createDo() {
+    if (!formData.action) return;
+    formData.donot = true;
+    formData.active = true;
+    console.log(formData)
+    await API.graphql({ query: createTodoMutation, variables: { input: formData }});
+    setTodos([ ...Todos, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function createDoNot() {
+    if (!formData.action) return;
+    formData.donot = false;
+    formData.active = true;
+    console.log(formData)
+    await API.graphql({ query: createTodoMutation, variables: { input: formData }});
+    setTodos([ ...Todos, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function updateTodo({ id }) {
+    const newTodosArray = Todos.filter(Todo => Todo.id !== id);
+    setTodos(newTodosArray);
+    await API.graphql({ query: updateTodoMutation, variables: { input: { id } }});
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href= "https://youtu.be/rn9lTpD-yKc?t=2525"><h1>Choose</h1></a>
+      <input
+        onChange={e => setFormData({ ...formData, 'action': e.target.value})}
+        placeholder="What do you want to do?"
+        value={formData.action}
+      />
+      <button onClick={createDo}>Aspire To Do</button>
+      <button onClick={createDoNot}>Aspire Not To Do</button>
+      <div style={{marginBottom: 30}}>
+        <h1>Do</h1>
+        {
+          Todos.filter(Todo => Todo.donot===true).map(Todo => (
+            <div key={Todo.id || Todo.action}>
+              <p>{Todo.action}</p>
+              <button onClick={() => updateTodo(Todo)}>Forget</button>
+            </div>
+          ))
+        }
+      </div>
+      <div style={{marginBottom: 30}}>
+        <h1>Don't Do</h1>
+        {
+          Todos.filter(Todo => Todo.donot===false).map(Todo => (
+            <div key={Todo.id || Todo.action}>
+              <p>{Todo.action}</p>
+              <button onClick={() => updateTodo(Todo)}>Forget</button>
+            </div>
+          ))
+        }
+      </div>
       <AmplifySignOut />
     </div>
+    
   );
 }
 
---
+++ src/graphql/queries.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getTodo = /* GraphQL */ `
+  query GetTodo($id: ID!) {
+    getTodo(id: $id) {
+      id
+      donot
+      action
+      active
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listTodos = /* GraphQL */ `
+  query ListTodos(
+    $filter: ModelTodoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        donot
+        action
+        active
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
